4912d48665ca8c0bd14112bfa858dd815d13e376
gabbene23 <gabbene23@gmail.com>
2014-04-15 12:15:51 -0400

    4/15
    Notes being put on site

---
+++ Lab 10/4_15 Notes.sql
@@ -0,0 +1,33 @@
+﻿-- Greg Abbene
+-- Professor Labouseur
+-- April 15th 2014	
+-- Lab 10	
+
+-- Store Procedures and Trigger
+-- LOOK @ Some PostgreSQL stored procs on site for template
+
+-- Create and INstert --> Adds 4 courses and has preReq chains...
+
+-- 1) Takes Parameter of Course (say 308) and write query to find out preReqs for course
+	-- @ one level back --> Should get closest preReq **should be 120
+
+-- Template
+CREATE OR REPLACE function test(int, refcursor) returns 
+refcursor as
+$$
+DECLARE 
+	creditnum INT  := $1;
+	resultset refcursor := $2;
+BEGIN
+	open resultset FOR
+	SELECT * 
+	FROM courses 
+	WHERE  credits > creditnum; 
+  RETURN resultset;
+END;
+$$ 
+LANGUAGE plpgsql;
+
+select test(2)
+-- 2) Say course (220) tells you what it is a preReq for @ 1 level back
+	-- It is preReq for 221
---
+++ Lab 10/Pre-Req.sql
@@ -0,0 +1,64 @@
+﻿----------------------------------------------------------------------------------------
+-- Courses and Prerequisites
+-- by Alan G. Labouseur
+-- Tested on Postgres 9.3.2
+----------------------------------------------------------------------------------------
+
+--
+-- The table of courses.
+--
+create table Courses (
+    num      integer not null,
+    name     text    not null,
+    credits  integer not null,
+  primary key (num)
+);
+
+
+insert into Courses(num, name, credits)
+values (499, 'CS/ITS Capping', 3 );
+
+insert into Courses(num, name, credits)
+values (308, 'Database Systems', 4 );
+
+insert into Courses(num, name, credits)
+values (221, 'Software Development Two', 4 );
+
+insert into Courses(num, name, credits)
+values (220, 'Software Development One', 4 );
+
+insert into Courses(num, name, credits)
+values (120, 'Introduction to Programming', 4);
+
+select * 
+from courses
+order by num ASC;
+
+
+--
+-- Courses and their prerequisites
+--
+create table Prerequisites (
+    courseNum integer not null references Courses(num),
+    preReqNum integer not null references Courses(num),
+  primary key (courseNum, preReqNum)
+);
+
+insert into Prerequisites(courseNum, preReqNum)
+values (499, 308);
+
+insert into Prerequisites(courseNum, preReqNum)
+values (499, 221);
+
+insert into Prerequisites(courseNum, preReqNum)
+values (308, 120);
+
+insert into Prerequisites(courseNum, preReqNum)
+values (221, 220);
+
+insert into Prerequisites(courseNum, preReqNum)
+values (220, 120);
+
+select *
+from Prerequisites
+order by courseNum DESC;
