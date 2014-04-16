709d3a883ad9544ee218f1669f7336a37bbd333a
Greg Abbene <gabbene23@gmail.com>
2014-04-16 14:58:06 -0400

    Failing at Lab 10

---
+++ Lab 10/Lab 10.sql
@@ -0,0 +1,40 @@
+﻿-- Greg Abbene
+-- Professor Labouseur
+-- April 15th 2014	
+-- Lab 10	
+
+-- 1) function PreReqsFor(courseNum) - Returns immediate prerequisites for the passed-in course number
+CREATE OR REPLACE function PreReqsFor(integer, refcursor) RETURNS 
+refcursor AS $$
+DECLARE 
+	courseNum INTEGER := $1;
+	resultset refcursor  := $2;
+BEGIN
+	open resultset FOR
+	SELECT p.preReqNum as "Prerequisite Number"
+	FROM Prerequisites p
+	WHERE  p.num = $1;
+  RETURN resultset;
+END
+$$ LANGUAGE plpgsql;
+
+SELECT PreReqsFor(308);
+
+-- 2) function IsPreReqsFor(courseNum) - Returns the course for which the psased-in course
+-- number is an immediate pre-requiste.
+
+CREATE OR REPLACE function IsPreReqsFor(integer, refcursor) RETURNS 
+refcursor AS $$
+DECLARE 
+	courseNum INTEGER := $1;
+	resultset refcursor  := $2;
+BEGIN
+	open resultset FOR
+	SELECT p.num as "Course Number"
+	FROM Prerequisites p
+	WHERE  p.preReqNum = $1;
+  RETURN resultset;
+END
+$$ LANGUAGE plpgsql;
+
+SELECT IsPreReqsFor(308);
